9bd7d77305ad2ee0152de82c1123f805fa3d26bd
rubamhassan <rubamhassan@gmail.com>
2016-12-19 13:49:33 -0800

    final commit

--- static/calendar.js
+++ static/calendar.js
@@ -1,9 +1,10 @@
 "use strict";
 
-layOutDay (events) { }
 
 var events=[ {start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670} ];
 
-function layOutDay {
-	console.log('test');
-}
+var layOutDay = function() {
+    for(var i=0; i < events.length; i++) {
+       console.log(events[i]);
+   }
+};
